5a707cb017fe5b835a8f5c64cea9d66c6dbac5fd
Ch4rs <campeador.david@gmail.com>
2019-05-23 11:22:54 -0700

    edit

--- classes/Manager.php
+++ classes/Manager.php
@@ -86,13 +86,13 @@ class Manager extends ConnectionManager
             }
         }
         $create = "INSERT INTO $table($camp)VALUES ($values)";
-        //echo "<br>$create</br>";
+        echo "<br>$create</br>";
 
     }
     private function generate_read($table)
     {
         $read = "SELECT * FROM $table";
-        //echo "<br>$read</br>";
+        echo "<br>$read</br>";
     }
     private function generate_update($table, $columns)
     {
@@ -106,15 +106,15 @@ class Manager extends ConnectionManager
                 $camps =$camps . $columns[$i] . "=:".$columns[$i].",";
             }
         }
-        $update = "UPDATE $table SET $camps WHERE $condition";
-        //echo "<br>$update</br>";
+        $update = "UPDATE $table SET $camps  WHERE $condition";
+        echo "<br>$update</br>";
 
     }
     private function generate_delete($table, $columns)
     {
         $id = $columns[0];
         $delete = "DELETE FROM $table WHERE $id=:$id";
-        //echo "<br>$delete</br>";
+        echo "<br>$delete</br>";
     }
 }
 function PRUEBAS()
