c9380100f89ea11a076575e3db216ba68a6586f7
Ch4rs <campeador.david@gmail.com>
2019-05-25 10:29:11 -0700

    Update main.js

--- chiefs/main.js
+++ chiefs/main.js
@@ -1,88 +1,89 @@
-$('#tb_chiefs_update').validate({
-    language: 'es',
-    errorClass: 'invalid',
-    validClass: 'valid',
-    rules: {
-        id_chief_update: {
-            required: true,
-
-        },
-        id_title_update: {
-            required: true,
-
-        },
-        chief_name_update: {
-            required: true,
-
-        },
-    },
-    messages: {
-        id_chief_update: {
-            required: true,
-
-        },
-        id_title_update: {
-            required: true,
-
-        },
-        chief_name_update: {
-            required: true,
-
-        },
-    },
-    honkeyup: false,
-    submitHandler: function() {
-        $('div.error').hide();
-        update();
-    },
-    honkeyup: false,
-    highlight: function(element, required) {
-        $(element).fadeOut(function() {
-            $(element).fadeIn();
-            $(element).css('border', '2px solid #FDADAF');
-        });
-    },
-    unhighlight: function(element, errorClass, validClass) {
-        $(element).css('border', '1px solid #CCC');
-    }
-});
-$('#tb_chiefs_create').validate({
-    language: 'es',
-    errorClass: 'invalid',
-    validClass: 'valid',
-    rules: {
-        id_title_create: {
-            required: true,
-
-        },
-        chief_name_create: {
-            required: true,
-
-        },
-    },
-    messages: {
-        id_title_create: {
-            required: true,
-
-        },
-        chief_name_create: {
-            required: true,
-
-        },
-    },
-    honkeyup: false,
-    submitHandler: function() {
-        $('div.error').hide();
-        update();
-    },
-    honkeyup: false,
-    highlight: function(element, required) {
-        $(element).fadeOut(function() {
-            $(element).fadeIn();
-            $(element).css('border', '2px solid #FDADAF');
-        });
-    },
-    unhighlight: function(element, errorClass, validClass) {
-        $(element).css('border', '1px solid #CCC');
-    }
-});
+$('#tb_chiefs_update').validate(
+                        {
+                            language: 'es',
+                            errorClass: 'invalid',
+                            validClass: 'valid',
+                            rules:
+                            {
+                                id_chief_update: {
+                        required: true, 
+                        
+                    },id_title_update: {
+                        required: true, 
+                        
+                    },chief_name_update: {
+                        required: true, 
+                        
+                    },
+                            },
+                            messages:
+                            {
+                                id_chief_update: {
+                        required: true, 
+                        
+                    },id_title_update: {
+                        required: true, 
+                        
+                    },chief_name_update: {
+                        required: true, 
+                        
+                    },
+                            },
+                            honkeyup: false,
+                            submitHandler: function () {
+                                $('div.error').hide();
+                                update();
+                            },
+                            honkeyup: false,
+                            highlight: function (element, required) {
+                                $(element).fadeOut(function () {
+                                    $(element).fadeIn();
+                                    $(element).css('border', '2px solid #FDADAF');
+                                });
+                            },
+                            unhighlight: function (element, errorClass, validClass) {
+                                $(element).css('border', '1px solid #CCC');
+                            }
+                        }
+                    );$('#tb_chiefs_create').validate(
+                        {
+                            language: 'es',
+                            errorClass: 'invalid',
+                            validClass: 'valid',
+                            rules:
+                            {
+                                id_title_create: {
+                        required: true, 
+                        
+                    },chief_name_create: {
+                        required: true, 
+                        
+                    },
+                            },
+                            messages:
+                            {
+                                id_title_create: {
+                        required: true, 
+                        
+                    },chief_name_create: {
+                        required: true, 
+                        
+                    },
+                            },
+                            honkeyup: false,
+                            submitHandler: function () {
+                                $('div.error').hide();
+                                update();
+                            },
+                            honkeyup: false,
+                            highlight: function (element, required) {
+                                $(element).fadeOut(function () {
+                                    $(element).fadeIn();
+                                    $(element).css('border', '2px solid #FDADAF');
+                                });
+                            },
+                            unhighlight: function (element, errorClass, validClass) {
+                                $(element).css('border', '1px solid #CCC');
+                            }
+                        }
+                    );
