5171a50db22516a073b31d5223357d21f18a2cd7
Ch4rs <campeador.david@gmail.com>
2019-05-29 16:41:32 -0700

    update

---
+++ users/delete.php
@@ -0,0 +1,37 @@
+<div class='modal' id='modal_delete' tabindex='-1' role='dialog'>
+                        <div class='modal-dialog' role='document'>
+                            <div class='modal-content'>
+                                <div class='modal-header'>
+                                    <h3 class='modal-title'>Eliminar Registro</h3>
+                                    <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                                        <span aria-hidden='true'>&times;</span>
+                                    </button>
+                                </div>
+                                <div class='modal-body'> 
+                                    
+            <form id='tb_users_delete' name='tb_users_delete'>
+                <div class='form-group'>
+                    
+                    <input type='hidden' id= 'id_user_delete' name= 'id_user_delete' class='form-control'>
+                </div><div class='form-group'>
+                    <label for='user_delete'>user</label>
+                    <input type='text' id= 'user_delete' name= 'user_delete' class='form-control'>
+                </div><div class='form-group'>
+                    <label for='name_delete'>name</label>
+                    <input type='text' id= 'name_delete' name= 'name_delete' class='form-control'>
+                </div><div class='form-group'>
+                    <label for='image_delete'>image</label>
+                    <input type='text' id= 'image_delete' name= 'image_delete' class='form-control'>
+                </div><div class='form-group'>
+                    <label for='password_delete'>password</label>
+                    <input type='text' id= 'password_delete' name= 'password_delete' class='form-control'>
+                </div>
+                <div class='modal-footer'>
+                    <button id='btn_delete' class='btn btn-primary'>Eliminar</button>
+                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Cancelar</button>
+                </div>
+            </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
---
+++ users/main.js
@@ -0,0 +1,369 @@
+$(document).ready(function () {
+                        $('#tb_users_update').validate(
+                        {
+                            language: 'es',
+                            errorClass: 'invalid',
+                            validClass: 'valid',
+                            rules:
+                            {
+                                id_user_update: {
+                             required:true
+                    },user_update: {
+                             required:true
+                    },name_update: {
+                             required:true
+                    },image_update: {
+                             required:true
+                    },password_update: {
+                             required:true
+                    },
+                            },
+                            messages:
+                            {
+                                id_user_update: {
+                        required:'Ingrese Id_user_update',
+                    },user_update: {
+                        required:'Ingrese User_update',
+                    },name_update: {
+                        required:'Ingrese Name_update',
+                    },image_update: {
+                        required:'Ingrese Image_update',
+                    },password_update: {
+                        required:'Ingrese Password_update',
+                    },
+                            },
+                            honkeyup: false,
+                            submitHandler: function () {
+                                $('div.error').hide();
+                                _update();
+                            },
+                            honkeyup: false,
+                            highlight: function (element, required) {
+                                $(element).fadeOut(function () {
+                                    $(element).fadeIn();
+                                    $(element).css('border', '2px solid #FDADAF');
+                                });
+                            },
+                            unhighlight: function (element, errorClass, validClass) {
+                                $(element).css('border', '1px solid #CCC');
+                            }
+                        }
+                    );$('#tb_users_create').validate(
+                        {
+                            language: 'es',
+                            errorClass: 'invalid',
+                            validClass: 'valid',
+                            rules:
+                            {
+                                user_create: {
+                             required:true
+                    },name_create: {
+                             required:true
+                    },image_create: {
+                             required:true
+                    },password_create: {
+                             required:true
+                    },
+                            },
+                            messages:
+                            {
+                                user_create: {
+                        required:'Ingrese User_create',
+                    },name_create: {
+                        required:'Ingrese Name_create',
+                    },image_create: {
+                        required:'Ingrese Image_create',
+                    },password_create: {
+                        required:'Ingrese Password_create',
+                    },
+                            },
+                            honkeyup: false,
+                            submitHandler: function () {
+                                $('div.error').hide();
+                                _create();
+                            },
+                            honkeyup: false,
+                            highlight: function (element, required) {
+                                $(element).fadeOut(function () {
+                                    $(element).fadeIn();
+                                    $(element).css('border', '2px solid #FDADAF');
+                                });
+                            },
+                            unhighlight: function (element, errorClass, validClass) {
+                                $(element).css('border', '1px solid #CCC');
+                            }
+                        }
+                    );$('#tb_users_delete').validate(
+                        {
+                            language: 'es',
+                            errorClass: 'invalid',
+                            validClass: 'valid',
+                            rules:
+                            {
+                                id_user_delete: {
+                             required:true
+                    },user_delete: {
+                             required:true
+                    },name_delete: {
+                             required:true
+                    },image_delete: {
+                             required:true
+                    },password_delete: {
+                             required:true
+                    },
+                            },
+                            messages:
+                            {
+                                id_user_delete: {
+                        required:'Ingrese Id_user_delete',
+                    },user_delete: {
+                        required:'Ingrese User_delete',
+                    },name_delete: {
+                        required:'Ingrese Name_delete',
+                    },image_delete: {
+                        required:'Ingrese Image_delete',
+                    },password_delete: {
+                        required:'Ingrese Password_delete',
+                    },
+                            },
+                            honkeyup: false,
+                            submitHandler: function () {
+                                $('div.error').hide();
+                                _delete(idDel);
+                            },
+                            honkeyup: false,
+                            highlight: function (element, required) {
+                                $(element).fadeOut(function () {
+                                    $(element).fadeIn();
+                                    $(element).css('border', '2px solid #FDADAF');
+                                });
+                            },
+                            unhighlight: function (element, errorClass, validClass) {
+                                $(element).css('border', '1px solid #CCC');
+                            }
+                        }
+                    );
+                        $('#btnModalCreate').click(function() {
+                            $('#modal_create').modal();
+                        });
+                        $('#btn_update').click(function () {
+                $('#tb_users_update').validate();
+            });$('#btn_create').click(function () {
+                $('#tb_users_create').validate();
+            });$('#btn_delete').click(function () {
+                $('#tb_users_delete').validate();
+            });
+                        
+                        alertas('Bienvenido', 'info');
+                        get();
+                        getSelect();
+                        getCatalogo();
+                    });function getCatalogo() {
+                                var URLactual = window.location.href;
+                                var catalog = URLactual.split('/')
+                                document.getElementById('Catalogo').innerHTML='Catálogo de '+ catalog[catalog.length -2];
+                            }function getSelect() {
+            
+        }function get() {
+                        $.post('main.php', { action: 'get' }, function (e) {
+                            if (e.error || !e.data) {
+                                alert(e.r);
+                            }
+                            else {
+                                setDataTable(e.r.c, e.r.d);
+                            }
+                        });
+                    }function FullSelect(nameselect, tabla) {
+                                var column = []
+                                $.post('main.php', { action: 'getselect', dt: tabla }, function (e) {
+                                    if (e.error || !e.data) {
+                                        alert(e.r);
+                                    }
+                                    else {
+                                        console.log(e)
+                                        column.push(e.r.c[0]['data'])
+                                        column.push(e.r.c[0]['title'])
+                                        setSelect(e.r.d, nameselect, column);
+                                    }
+                                });
+                            }
+                            function setSelect(array, nameselect, column) {
+            array.forEach(element => {
+                var o = new Option(element[column[1]], element[column[0]]);
+                $(nameselect).append(o);
+            });
+        }function setDataTable(c, d) {
+                    var options = {
+                        dom: 'Bfrtip',
+                        buttons: [{
+                            extend: 'collection',
+                            text: 'Control',
+                            orientation: 'landscape',
+                            buttons: [{
+                                text: 'Copiar',
+                                extend: 'copy',
+                                exportOptions: {
+                                    columns: [0,1,2,3,4]
+                                }
+                            }, {
+                                extend: 'pdf',
+                                orientation: 'landscape',
+                                pageSize: 'LEGAL',
+                                exportOptions: {
+                                    columns: [0,1,2,3,4]
+                                }
+                            },
+                            {
+                                text: 'Excel',
+                                extend: 'excel',
+                                exportOptions: {
+                                    columns: [0,1,2,3,4]
+                                }
+                            },
+                            {
+                                text: 'Imprimir',
+                                extend: 'print',
+                                exportOptions: {
+                                    columns: [0,1,2,3,4]
+                                }
+                            }
+                            ]
+                        },
+                        {
+                            extend: 'colvis',
+                            text: 'Visor de columnas',
+                            collectionLayout: 'fixed three-column'
+                        }
+                        ],
+                        autoWidth: true,
+                        columns: c,
+                        data: d,
+                        destroy: true,
+                        language: {
+                            sProcessing: 'Procesando...',
+                            sLengthMenu: 'Mostrar _MENU_ registros',
+                            sZeroRecords: 'No se encontraron resultados',
+                            sEmptyTable: 'Ningún dato disponible en esta tabla',
+                            sInfo: 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+                            sInfoEmpty: 'Mostrando registros del 0 al 0 de un total de 0 registros',
+                            sInfoFiltered: '(filtrado de un total de _MAX_ registros)',
+                            sInfoPostFix: '',
+                            sSearch: 'Buscar:',
+                            sUrl: '',
+                            sInfoThousands: ',',
+                            sLoadingRecords: 'Cargando...',
+                            oPaginate: {
+                                sFirst: 'Primero',
+                                sLast: 'Último',
+                                sNext: 'Siguiente',
+                                sPrevious: 'Anterior'
+                            },
+                            oAria: {
+                                sSortAscending: ': Activar para ordenar la columna de manera ascendente',
+                                sSortDescending: ': Activar para ordenar la columna de manera descendente'
+                            }
+                        },
+                        select: true,
+                        responsive:true,
+                    };
+                    $('#tblTabla').DataTable(options);
+                }function editx(id) {
+                    var datos = {
+                        id_user: id
+                    };
+                    $.post('main.php', { action: 'consult', dt: datos }, function(e) {
+                        if (e.error) {
+                            alertas(e.r, 'danger');
+                        } else {
+                            setDatosModal(e.r.d);
+                        }
+                    });
+                    return false;
+                }function setDatosModal(d) {
+                    $('#id_user_update').val(d[0]['id_user']);$('#user_update').val(d[0]['user']);$('#name_update').val(d[0]['name']);$('#image_update').val(d[0]['image']);$('#password_update').val(d[0]['password']);
+                    $('#modal_update').modal();
+                }
+
+                function seleccionarSelect(nameselect, valor) {
+                    $(nameselect).find('option').each(function(index, element) {
+                        if (element.value == valor) {
+                            $(nameselect).val(element.value);
+                        }
+                    });
+                }function _update() {
+                    var datos = {
+                        id_user: $('#id_user_update').val(),user: $('#user_update').val(),name: $('#name_update').val(),image: $('#image_update').val(),password: $('#password_update').val()
+                    };
+                    $.post('main.php', { action: 'update', dt: datos }, function(e) {
+                        if (e.error || !e.data) {
+                            alertas(e.r, 'danger');
+                        } else {
+                            alertas('Se ha editado Correctamente', 'success');
+                            get();
+                            $('#modal_update').modal('hide');
+                        }
+                    });
+                    
+                    return false;
+                }function _create() {
+                    var datos = {
+                        user: $('#user_create').val(),name: $('#name_create').val(),image: $('#image_create').val(),password: $('#password_create').val()
+                    };
+                    $.post('main.php', { action: 'set', dt: datos }, function(e) {
+                        if (e.error || !e.data) {
+                            alertas(e.r, 'danger');
+                        } else {
+                            alertas('Se ha registrado Correctamente', 'success');
+                            get();
+                            $('#modal_create').modal('hide');
+                        }
+                    });
+                    
+                    return false;
+                }function deletex(id) {
+                    var datos = {
+                        id_user: id
+                    };
+                    $.post('main.php', { action: 'consult', dt: datos }, function(e) {
+                        if (e.error) {
+                            alertas(e.r, 'danger');
+                        } else {
+                            setMensajeModal(e.r.d);
+                        }
+                    });
+                    return false;
+                }
+                var idDel = 0;function setMensajeModal(d) {
+                    $('#id_user_delete').val(d[0]['id_user']);$('#user_delete').val(d[0]['user']);$('#name_delete').val(d[0]['name']);$('#image_delete').val(d[0]['image']);$('#password_delete').val(d[0]['password']);
+                    $('#modal_delete').modal();
+                    idDel = d[0]['id_user'];
+
+                }function _delete(id) {
+                    var datos = {
+                        id_user: id
+                    };
+                    $.post('main.php', { action: 'del', dt: datos }, function(e) {
+                        if (e.error) {
+                            alertas(e.r, 'danger');
+                        } else {
+                            alertas('Registro Eliminado Correctamente', 'success');
+                            $('#modal_delete').modal('hide');
+                            get();
+                        }
+                    });
+                    return false;
+                }function cancelar(form) {
+                    $('#' + form)[0].reset();
+                    var validator = $('#' + form).validate();
+                    validator.resetForm();
+                    return false;
+                }function alertas(mensaje, tipo) {
+                    $.notify({
+                        // options
+                        message: mensaje
+                    }, {
+                        // settings
+                        type: tipo,
+                        delay: 1500
+                    });
+                }
